891fbabf0f1e6a1dd1f8ecc7b11d93981c25a902
Kaio Vinicius <kaioovinicc@gmail.com>
2026-03-23 15:58:12 -0300

    primeiro commit

---
+++ N3_NC1/n3_nc1_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int numeroGrande = 4000000000;
+    int numero = 4000000000;
+
+    printf("Número positivo grande: %lu\n", numeroGrandePositivo);
+    printf("Número positivo: %lu\n", numeroPositivo);
+    printf("Número grande: %lu\n", numeroGrande);
+    printf("Número: %d\n", numero);
+
+    return 0;
+    }
