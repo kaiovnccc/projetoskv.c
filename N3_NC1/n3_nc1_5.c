6364c9dfc274c6aadff1721929f1291bfa67f383
Kaio Vinicius <kaioovinicc@gmail.com>
2026-03-24 10:49:54 -0300

    Atualização do código

--- N3_NC1/n3_nc1_5.c
+++ N3_NC1/n3_nc1_5.c
@@ -7,9 +7,9 @@ int main() {
     numeroPequeno = 32768; // Valor maior que o máximo de short int
     printf("Número pequeno atualizado (short int): %d\n", numeroPequeno);
 
-    printf("\n*** Tamanho das variaveis ***\n");
-    printf("Short int: %lu B - int: %lu B - long int: %lu B\n", sizeof(short int), sizeof(int), sizeof(long int));
-    printf("Float: %lu B - Double: %lu B - long double: %lu B\n", sizeof(float), sizeof(double), sizeof(long double));
+    // printf("\n*** Tamanho das variaveis ***\n");
+    // printf("Short int: %lu B - int: %lu B - long int: %lu B\n", sizeof(short int), sizeof(int), sizeof(long int));
+    // printf("Float: %lu B - Double: %lu B - long double: %lu B\n", sizeof(float), sizeof(double), sizeof(long double));
 
     return 0;
     }
