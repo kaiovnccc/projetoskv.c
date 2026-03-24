22520aa15b33d8b82fc683a01efcffe9abc03037
Kaio Vinicius <kaioovinicc@gmail.com>
2026-03-24 13:18:43 -0300

    adicionando arquivos

---
+++ N3_NC2/n3_nc2_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x = 8;
+    float y = 8.0;
+    //char c = 'a';
+
+    printf("x >= y: %d\n", x >= y);
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+    
+    //printf("x >= c: %d\n", x >= c);
+    //printf("O valor ASCII de %c é %d: \n", c, c);
+}
---
+++ N3_NC2/n3_nc2_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+    printf("Número 1 > Numero 2: %d\n", (int)numero1 > numero2);
+    printf("Número 1 == Numero 2: %d\n", (int)numero1 == numero2);
+
+    return 0;
+}
